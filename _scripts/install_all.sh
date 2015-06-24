a7bb1d37a63f2c10f4b75ea707ad1eb65580a954
Renoir Boulanger <renoir@w3.org>
2015-06-24 16:30:19 -0700

    proposal

--- _scripts/install_all.sh
+++ _scripts/install_all.sh
@@ -1,43 +1,40 @@
 #!/bin/bash -ex
 
 # Clone all the projects
+echo "Clone all repos"
 
-git clone https://github.com/mozilla/fxa-content-server.git &
-git clone https://github.com/mozilla/fxa-auth-server.git &
-git clone https://github.com/vladikoff/browserid-verifier.git -b http &
-
-git clone https://github.com/mozilla/fxa-oauth-server.git &
-git clone https://github.com/mozilla/fxa-oauth-console.git &
-
-git clone https://github.com/mozilla/fxa-profile-server.git &
-
-git clone https://github.com/mozilla/123done.git -b oauth &
-
-git clone https://github.com/mozilla-services/loop-server.git &
-
-git clone https://github.com/mozilla-services/syncserver.git &
+bash _scripts/clone_all.sh
 
 wait
 
-# Install and Setup all the projects
+echo "Setup all the projects"
 
-
+echo " * fxa-content-server"
 cd fxa-content-server; npm i --production; npm i; cp server/config/local.json-dist server/config/local.json; cd ..
 
+echo " * fxa-auth-server"
 cd fxa-auth-server; npm i; node ./scripts/gen_keys.js; cd ..
 # Install a custom http only verifier
+
+echo " * browserid-verifier"
 cd browserid-verifier; npm i; npm i vladikoff/browserid-local-verify#http; cd ..
 
+echo " * fxa-oauth-server"
 cd fxa-oauth-server; npm i; cd ..
 
+echo " * fxa-oauth-console"
 cd fxa-oauth-console; npm i; cd ..
 
+echo " * fxa-profile-server"
 cd fxa-profile-server; npm i; mkdir -p var/public/; cd ..
 
+echo " * 123done"
 cd 123done; npm i; CONFIG_123DONE=./config-local.json node ./scripts/gen_keys.js; cd ..
 
+echo " * loop-server"
 cd loop-server; npm i; cd ..
 
+echo " * syncserver"
 cd syncserver; make build; cd ..
 
 ln -sf node_modules/.bin/pm2 pm2
