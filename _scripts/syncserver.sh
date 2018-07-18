0d79d2f2e8004b593fbb83210e1a3154b206b136
Ryan Kelly <rfkelly@mozilla.com>
2018-07-18 10:32:56 +1000

    Detect host address based on docker platform.

--- _scripts/syncserver.sh
+++ _scripts/syncserver.sh
@@ -1,10 +1,18 @@
 #!/bin/sh -ex
 
-docker run --rm --name syncserver \
+DOCKER_OS="`docker info --format '{{.OperatingSystem}}'`"
+
+if [ "$DOCKER_OS" = 'Docker for Windows' -o "$DOCKER_OS" = 'Docker for Mac' ]; then
+  HOST_ADDR='host.docker.internal'
+else
+  HOST_ADDR='127.0.0.1'
+fi
+
+docker run --net=host --rm --name syncserver \
   -p 5000:5000 \
   -e SYNCSERVER_PUBLIC_URL=http://localhost:5000 \
-  -e SYNCSERVER_IDENTITY_PROVIDER=http://host.docker.internal:3030 \
-  -e SYNCSERVER_BROWSERID_VERIFIER=http://host.docker.internal:5050 \
+  -e SYNCSERVER_IDENTITY_PROVIDER=http://$HOST_ADDR:3030 \
+  -e SYNCSERVER_BROWSERID_VERIFIER=http://$HOST_ADDR:5050 \
   -e SYNCSERVER_SECRET=5up3rS3kr1t \
   -e SYNCSERVER_SQLURI=sqlite:////tmp/syncserver.db \
   -e SYNCSERVER_BATCH_UPLOAD_ENABLED=true \
